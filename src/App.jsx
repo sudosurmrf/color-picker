4106d4b34b720ee49878f6fade77bbd35fdefd3b
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2024-07-17 10:47:36 -0700

    added the color picker function to properly display the correct clicked color

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 // Write your Color component here
-const Color = ({ color }) => {
-  return <div className = {color}>
+import { useState } from 'react';
+
+const Color = ({ color, setSelectedColor }) => {
+  return <div className = {color} 
+  onClick={() => {
+    setSelectedColor(color)
+  }}>
     
   </div>
 };
 
-const App = () => {
+const Picker = () => {
+  const [selectedColor, setSelectedColor] = useState('');
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
         <div id="colors-list">
-          <Color color='green'/>
-          <Color color='orange'/>
-          <Color color='violet'/>
+          <Color color='green' setSelectedColor={setSelectedColor}/>
+          <Color color='orange'setSelectedColor={setSelectedColor}/>
+          <Color color='violet'setSelectedColor={setSelectedColor}/>
         </div>
     </div>
   );
 };
 
-export default App;
+export default Picker;
